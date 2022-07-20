d58eebbb6e50e680a0f154a9924545ace0e3a6b7
DESKTOP-8HJUV4A\jodan <mofangjodan@gmail.com>
2022-07-20 15:03:59 +0800

    遊戲資訊頁新增遊戲新聞，新聞頁面新增遊戲連結

--- app/Views/pages/gameinfo.php
+++ app/Views/pages/gameinfo.php
@@ -5,6 +5,8 @@ $AAA1='https://api.mofang.com.tw/gameinfo.php?userid&gameid=';
 $url0_gameinfo1=$AAA1.$game_pagesinfo1;
 $json0_gameinfo1 = file_get_contents($url0_gameinfo1);
 $json0_gameinfo1 = json_decode($json0_gameinfo1);
+$LOGO = $json0_gameinfo1 -> results -> LOGO;
+
 $GAME_NAME = $json0_gameinfo1 -> results -> GAME_NAME;
 $GAME_INFO = $json0_gameinfo1 -> results -> GAME_INFO;
 $LOGO = $json0_gameinfo1 -> results -> LOGO;
@@ -24,63 +26,99 @@ $Officialwebsite = $json0_gameinfo1 -> results -> Officialwebsite;
 <div class="row">
     <div class="col-sm-1" >
             <div class="row">
-
                 
             </div>
     </div>
-<div class="col-sm-10" >
+    <div class="col-sm-10" >
+        <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <img src= <?php print($LOGO);?> width="20%">
+                    <?php print($GAME_NAME);?>
+                    </div>
+                </div>
+        </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src= <?php print($PIC0);?> width=100%>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src= <?php print($PIC1);?> width=100%>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src= <?php print($PIC2);?> width=100%>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src= <?php print($PIC3);?> width=100%>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src= <?php print($PIC4);?> width=100%>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12">                
+                    <?php print($GAME_INFO);?>
+                        <a href=<?php print($Officialwebsite); ?>><b>官網</b></a>
+                </div>
+            </div>
+<!-- 遊戲相關新聞 -->
+<h2>遊戲相關新聞 </h2>
 <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <img src= <?php print($LOGO);?> width="20%">
-                <?php print($GAME_NAME);?>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        <img src= <?php print($PIC0);?> width=100%>
+<?php
+$About_Game_news="https://api.mofang.com.tw/about_game_news.php?gameid=";
+$ABGN=$About_Game_news.$game_pagesinfo1;
+$json0_gameallnews1 = file_get_contents($ABGN);
+$json0_gameallnews1 = json_decode($json0_gameallnews1);
+for ($cc=0;$cc<5;$cc++)
+{
+$MOFANGNAME = $json0_gameallnews1 -> MOFANGNEWS[$cc] -> MOFANGNAME;
+$MOFANGNEWSID = $json0_gameallnews1 -> MOFANGNEWS[$cc] -> MOFANGNEWSID;
+$MOFANGNEWSTITLE = $json0_gameallnews1 -> MOFANGNEWS[$cc] -> MOFANGNEWSTITLE;
+$MOFANGNEWSURL = $json0_gameallnews1 -> MOFANGNEWS[$cc] -> MOFANGNEWSURL;
+$MOFANGIMG = $json0_gameallnews1 -> MOFANGNEWS[$cc] -> MOFANGIMG;
+$MOFANGNEWSDATE = $json0_gameallnews1 -> MOFANGNEWS[$cc] -> MOFANGNEWSDATE;
+$MOFANGNEWSINFO = $json0_gameallnews1 -> MOFANGNEWS[$cc] -> MOFANGNEWSINFO;
+?>
+
+            <!-- <div class="row"> -->
+                <?php if($MOFANGIMG !=''){?>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <a href= ../news/<?php print($MOFANGNEWSID);?>>
+                            <img src= <?php print($MOFANGIMG);?> width=100%>
+                            <br>
+                            <?php print($MOFANGNEWSINFO);?></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        <img src= <?php print($PIC1);?> width=100%>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        <img src= <?php print($PIC2);?> width=100%>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        <img src= <?php print($PIC3);?> width=100%>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="card">
-                    <div class="card-body">
-                        <img src= <?php print($PIC4);?> width=100%>
-                    </div>
-                </div>
+                <?php } ?>
+                <?php } ?>
             </div>
 
-        </div>
+<!-- 遊戲相關新聞 -->
 
-        <div class="row">
-            <div class="col-sm-12">                
-                <?php print($GAME_INFO);?>
-                    <a href=<?php print($Officialwebsite); ?>><b>官網</b></a>
-            </div>
-        </div>
     </div>
+
 </div>
--- app/Views/pages/news.php
+++ app/Views/pages/news.php
@@ -1,17 +1,26 @@
 <p>
 <?php
-$uri = service('uri');
-$news_pagesinfo= $uri->getSegment(2);
-$AA='https://api.mofang.com.tw/mofang_news.php?mofangnews=';
-if($news_pagesinfo != '')
-{
-$url0_newsinfo=$AA.$news_pagesinfo;
-$json0_newsinfo = file_get_contents($url0_newsinfo);
-$json0_newsinfo = json_decode($json0_newsinfo);
-$MOFANGNEWSID = $json0_newsinfo -> results[0] -> MOFANGNEWSID;
-$MOFANGNEWSTITLE = $json0_newsinfo -> results[0] -> MOFANGNEWSTITLE;
-$MOFANGIMG = $json0_newsinfo -> results[0] -> MOFANGIMG;
-$MOFANGCONNET = $json0_newsinfo -> results[0] -> MOFANGCONNET;
+    $uri = service('uri');
+    $news_pagesinfo= $uri->getSegment(2);
+    $AA='https://api.mofang.com.tw/mofang_news.php?mofangnews=';
+    if($news_pagesinfo != '')
+    {
+        
+    $url0_newsinfo=$AA.$news_pagesinfo;
+    $json0_newsinfo = file_get_contents($url0_newsinfo);
+    $json0_newsinfo = json_decode($json0_newsinfo);
+    $MOFANGNEWSID = $json0_newsinfo -> results[0] -> MOFANGNEWSID;
+    $MOFANGNEWSTITLE = $json0_newsinfo -> results[0] -> MOFANGNEWSTITLE;
+    $MOFANGIMG = $json0_newsinfo -> results[0] -> MOFANGIMG;
+    $MOFANGCONNET = $json0_newsinfo -> results[0] -> MOFANGCONNET;
+    $GAME_ID =  $json0_newsinfo -> GAME_INFO[0] -> GAME_ID;
+    $GAME_NAME =  $json0_newsinfo -> GAME_INFO[0] -> GAME_NAME;
+    $GAME_IMG =  $json0_newsinfo -> GAME_INFO[0] -> GAME_IMG;
+    $IOS_PLATFORM =  $json0_newsinfo -> GAME_INFO[0] -> IOS_PLATFORM;
+    $ANDROID_PLATFORM =  $json0_newsinfo -> GAME_INFO[0] -> ANDROID_PLATFORM;
+
+
+
 ?>
 
 <div class="row">
@@ -19,25 +28,34 @@ $MOFANGCONNET = $json0_newsinfo -> results[0] -> MOFANGCONNET;
             <div class="row">
             </div>
     </div>
-<div class="col-sm-10" >
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                <h1><?= esc($MOFANGNEWSTITLE);?></h1>
-                </div>
-            <!-- </div>
+    <div class="col-sm-10" >
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                    <h3><?= esc($MOFANGNEWSTITLE);?></h3>
+                    </div>
+                <!-- </div>
+            </div>
         </div>
-    </div>
 
-    <div class="col-sm-10">
-        <div class="row">
-            <div class="col-sm-12"> -->
-                <div class="card">
-                    <?php print($MOFANGCONNET); ?>
+        <div class="col-sm-10">
+            <div class="row">
+                <div class="col-sm-12"> -->
+                    <div class="card">
+                        <?php print($MOFANGCONNET); ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+
+        <div class="col-sm-1" >
+                <div class="row">
+                    <a href= ../gameinfo/<?php print($GAME_ID);?> >
+                    <img src = <?php print($GAME_IMG);?> width="100%" >
+                    <b><?php print($GAME_NAME);?></b></a>
+                </div>
+        </div>
+
 </div>
 <?php
 
